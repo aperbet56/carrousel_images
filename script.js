deac0a15debac87482b476321c67834c18a2b21c
Annabelle Perbet <annabelleperbet@yahoo.fr>
2023-11-12 16:06:23 +0100

    Création de la variable count + amélioration des fonctions nextImage et prevImage afin d'avoir un indicateur (count) dynamique

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 const allImg = document.querySelectorAll("img");
+const indicator = document.querySelector(".carousel__indicator");
 const numberSlider = allImg.length;
 
-// Création de la variable index
+// Création de la variable index et de la variable count
 let index = 0;
+let count = 1;
 
 // Déclaration de la fonction nextImage qui va permettre de passer à l'image suivante
 const nextImage = () => {
@@ -13,12 +15,15 @@ const nextImage = () => {
 
   if (index < numberSlider - 1) {
     index++;
+    count++;
+    indicator.textContent = `${count} / ${numberSlider}`;
   } else {
     index = 0;
+    count = 1;
+    indicator.textContent = `${count} / ${numberSlider}`;
   }
-
   allImg[index].classList.add("active");
-  console.log(index);
+  //console.log(index);
 };
 
 // Déclaration de la fonction prevImage qui va permettre de revenir à l'image précédente
@@ -27,12 +32,16 @@ const prevImage = () => {
 
   if (index > 0) {
     index--;
+    count--;
+    indicator.textContent = `${count} / ${numberSlider}`;
   } else {
     index = numberSlider - 1;
+    count = numberSlider;
+    indicator.textContent = `${count} / ${numberSlider}`;
   }
 
   allImg[index].classList.add("active");
-  console.log(index);
+  //console.log(index);
 };
 
 // Ecoute des événements "click" sur les boutons et appel des fonctions
